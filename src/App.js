9f49fd06a39c769fe5defcf2c916d1e78758ba18
av <45238682+avxlji@users.noreply.github.com>
2020-07-28 10:28:45 -0400

    updated 404 page routing

--- src/App.js
+++ src/App.js
@@ -1,63 +1,46 @@
 import React from 'react';
-import './styles/App.css'
-import Landing from './components/Landing'
-import About from './components/About'
-import FlippedWave from './components/FlippedWave'
-import Project from './components/Projects'
-import Contact from './components/Contact'
-import Apply from './components/Apply'
+import './styles/App.css';
+import Landing from './components/Landing';
+import About from './components/About';
+import FlippedWave from './components/FlippedWave';
+import Project from './components/Projects';
+import Contact from './components/Contact';
+import Apply from './components/Apply';
 /*
 import Skills from './components/Skills'
 */
-import Navbar from './components/Navbar'
-import FlippedTop from './components/FlippedTop'
-import RouteNotFound from './components/RouteNotFound'
- 
-import {
- BrowserRouter as Router,
- Switch,
- Route,
- Link
-} from "react-router-dom";
- 
- 
- 
- 
+import Navbar from './components/Navbar';
+import FlippedTop from './components/FlippedTop';
+import RouteNotFound from './components/RouteNotFound';
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
- return (
-   <>
- 
-{/*<FlippedTop></FlippedTop>*/}
- 
-<Router>
-<Switch>
+  return (
+    <>
+      {/*<FlippedTop></FlippedTop>*/}
 
-<Route exact path='/'>
-<Navbar></Navbar>
-<div className='sample' id='listen'>
-<FlippedTop></FlippedTop>
-<Landing></Landing>
-<About id='about'></About>
-<Project></Project>
-{/*
-<Skills></Skills>
-*/}
-<Contact></Contact>
-<FlippedWave></FlippedWave>
-</div>
-</Route>
- 
-<Route exact path='/apply'>
-<Apply></Apply>
-</Route>
-<Route path='*' component={RouteNotFound} />
-</Switch>
-</Router>
- 
-</>
- );
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Navbar></Navbar>
+            <div className="sample" id="listen">
+              <FlippedTop></FlippedTop>
+              <Landing></Landing>
+              <About id="about"></About>
+              <Project></Project>
+              <Contact></Contact>
+              <FlippedWave></FlippedWave>
+            </div>
+          </Route>
+          <Route exact path="/apply">
+            <Apply></Apply>
+          </Route>
+          <Route component={RouteNotFound} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
- 
-export default App;
- 
 
+export default App;
