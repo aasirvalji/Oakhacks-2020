2d4082efeaef6f0c69ecf6be1f1bebfe2b366017
av <45238682+avxlji@users.noreply.github.com>
2020-08-19 14:29:42 -0400

    removed dev schedule, added updated schedule component and moved events data to json file

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../styles/Schedule.css';
 import Slide from 'react-reveal/Slide';
 
+import data from '../data/events.json';
+
 import Paper from '@material-ui/core/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
@@ -10,22 +12,26 @@ import {
   Appointments,
   AppointmentTooltip,
   AppointmentForm,
+  AppointmentsProps,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
+const Appointment = ({ children, style, ...restProps }) => (
+  <Appointments.Appointment
+    {...restProps}
+    style={{
+      ...style,
+      backgroundColor: '#000088',
+      borderRadius: '8px',
+      borderColor: '#7FDBFF',
+      boxShadow:
+        '0 9.5px 19px rgba(0, 0, 0, 0.3), 0 7.5px 6px rgba(0, 0, 0, 0.22)',
+    }}
+  >
+    {children}
+  </Appointments.Appointment>
+);
+
 const Schedule = () => {
-  const currentDate = '2020-09-12';
-  const schedulerData = [
-    {
-      startDate: '2020-09-12T09:45',
-      endDate: '2020-09-12T11:00',
-      title: 'Meeting',
-    },
-    {
-      startDate: '2020-09-12T12:00',
-      endDate: '2020-09-12T13:30',
-      title: 'Go to a gym',
-    },
-  ];
   return (
     <div className="schedule">
       <Slide right>
@@ -33,11 +39,10 @@ const Schedule = () => {
       </Slide>
       <div className="schedule-container">
         <Paper>
-          <Scheduler data={schedulerData}>
-            <ViewState currentDate={currentDate} />
-            <DayView startDayHour={9} endDayHour={24} intervalCount={2} />
-            <Appointments />
-            {/* <AppointmentTooltip showCloseButton showOpenButton={false} /> */}
+          <Scheduler data={data.events} height={660}>
+            <ViewState currentDate={data.currentDate} />
+            <DayView startDayHour={0} endDayHour={24} intervalCount={2} />
+            <Appointments appointmentComponent={Appointment} />
           </Scheduler>
         </Paper>
       </div>
